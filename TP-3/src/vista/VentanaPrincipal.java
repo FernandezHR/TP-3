dd32ae0470390c892d2327a000b659113c726161
Usuario <Usuario@Usuario-PC>
2019-10-19 20:38:44 -0300

    Cambio visual

--- TP-3/src/vista/VentanaPrincipal.java
+++ TP-3/src/vista/VentanaPrincipal.java
@@ -16,6 +16,9 @@ import javax.swing.SwingConstants;
 import javax.swing.JSpinner;
 import javax.swing.ImageIcon;
 import javax.swing.SpinnerNumberModel;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class VentanaPrincipal extends JFrame
 {
@@ -26,12 +29,18 @@ public class VentanaPrincipal extends JFrame
 	public JSpinner cantProgramador, cantLiderDeProyecto, cantArquitecto, cantTester;
 	public JLabel lblCantNombres;
 
+	private Color verde, verde2;
+	
 	public VentanaPrincipal() 
 	{
-		setBounds(100, 100, 600, 546);
+		verde = new Color(21, 182, 141);
+		verde2 = new Color(16, 163, 125);
+		
+		setBounds(100, 100, 750, 546);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
+		getContentPane().setBackground(verde);
 		
 		initialize();
 	}
@@ -39,38 +48,31 @@ public class VentanaPrincipal extends JFrame
 	private void initialize() 
 	{
 		JPanel panelCrearEmpleado = new JPanel();
-		panelCrearEmpleado.setBackground(Color.LIGHT_GRAY);
-		panelCrearEmpleado.setBounds(10, 11, 189, 178);
-		getContentPane().add(panelCrearEmpleado);
+		panelCrearEmpleado.setBackground(verde2);
+		panelCrearEmpleado.setBounds(10, 22, 218, 178);
 		panelCrearEmpleado.setLayout(null);
+		getContentPane().add(panelCrearEmpleado);
 		
 		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblNombre.setForeground(Color.WHITE);
 		lblNombre.setBounds(10, 14, 50, 30);
 		panelCrearEmpleado.add(lblNombre);
 		
 		JLabel lblApellido = new JLabel("Apellido:");
+		lblApellido.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblApellido.setForeground(Color.WHITE);
 		lblApellido.setBounds(10, 55, 50, 30);
 		panelCrearEmpleado.add(lblApellido);
 		
 		JLabel lblPuesto = new JLabel("Puesto:");
+		lblPuesto.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblPuesto.setForeground(Color.WHITE);
 		lblPuesto.setBounds(10, 96, 50, 30);
 		panelCrearEmpleado.add(lblPuesto);	
 		
-		txtApellido = new JTextField();
-		txtApellido.setBounds(70, 55, 100, 30);
-		txtApellido.addKeyListener(new KeyAdapter() 
-		{
-			@Override
-			public void keyTyped(KeyEvent arg0) 
-			{
-				if(!Character.isLetter(arg0.getKeyChar()))
-					arg0.consume();
-			}
-		});
-		panelCrearEmpleado.add(txtApellido);
-		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(70, 14, 100, 30);
+		txtNombre.setBounds(70, 14, 138, 30);
 		txtNombre.addKeyListener(new KeyAdapter() 
 		{
 			@Override
@@ -82,74 +84,159 @@ public class VentanaPrincipal extends JFrame
 		});
 		panelCrearEmpleado.add(txtNombre);
 		
+		txtApellido = new JTextField();
+		txtApellido.setBounds(70, 55, 138, 30);
+		txtApellido.addKeyListener(new KeyAdapter() 
+		{
+			@Override
+			public void keyTyped(KeyEvent arg0) 
+			{
+				if(!Character.isLetter(arg0.getKeyChar()))
+					arg0.consume();
+			}
+		});
+		panelCrearEmpleado.add(txtApellido);
+		
 		cmboxPuestos = new JComboBox<String>();
-		cmboxPuestos.setBounds(70, 96, 100, 30);
-		panelCrearEmpleado.add(cmboxPuestos);
+		cmboxPuestos.setBounds(70, 96, 138, 30);
 		cmboxPuestos.setModel(new DefaultComboBoxModel<String>(new String[] {null, "Lider de Proyecto", "Arquitecto", "Programador", "Tester"}));
+		panelCrearEmpleado.add(cmboxPuestos);
 	
 		btnCargar = new JButton("Cargar");
-		btnCargar.setBounds(49, 137, 86, 30);
+		btnCargar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) 
+			{
+				btnCargar.setFont(new Font("Nirmala UI", Font.BOLD, 12));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) 
+			{
+				btnCargar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+			}
+		});
+		btnCargar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+		btnCargar.setBounds(59, 137, 107, 30);
+		btnCargar.setBackground(verde2.darker());
+		btnCargar.setForeground(Color.WHITE);
+		btnCargar.setFocusable(false);
 		panelCrearEmpleado.add(btnCargar);
 		
-		btnEliminar = new JButton("Eliminar");
-		btnEliminar.setBounds(238, 467, 86, 30);
-		btnEliminar.setEnabled(false);
-		getContentPane().add(btnEliminar);
-		
 		JPanel panelGenerarEmpleado = new JPanel();
-		panelGenerarEmpleado.setBackground(Color.LIGHT_GRAY);
-		panelGenerarEmpleado.setBounds(372, 11, 202, 178);
+		panelGenerarEmpleado.setBackground(verde2);
+		panelGenerarEmpleado.setBounds(10, 211, 218, 178);
 		panelGenerarEmpleado.setLayout(null);
 		getContentPane().add(panelGenerarEmpleado);
 		
 		JLabel lblLiderDeProyecto = new JLabel("Lider de Proyecto");
-		lblLiderDeProyecto.setBounds(10, 11, 93, 21);
+		lblLiderDeProyecto.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblLiderDeProyecto.setForeground(Color.WHITE);
+		lblLiderDeProyecto.setBounds(10, 11, 95, 21);
 		panelGenerarEmpleado.add(lblLiderDeProyecto);
 		
 		JLabel lblArquitecto = new JLabel("Arquitecto");
-		lblArquitecto.setBounds(113, 11, 93, 21);
+		lblArquitecto.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblArquitecto.setForeground(Color.WHITE);
+		lblArquitecto.setBounds(125, 11, 95, 21);
 		panelGenerarEmpleado.add(lblArquitecto);
 		
 		JLabel lblProgramador = new JLabel("Programador");
-		lblProgramador.setBounds(10, 59, 93, 21);
+		lblProgramador.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblProgramador.setForeground(Color.WHITE);
+		lblProgramador.setBounds(10, 59, 95, 21);
 		panelGenerarEmpleado.add(lblProgramador);
 		
 		JLabel lblTester = new JLabel("Tester");
-		lblTester.setBounds(113, 59, 93, 21);
+		lblTester.setFont(new Font("Nirmala UI", Font.BOLD, 11));
+		lblTester.setForeground(Color.WHITE);
+		lblTester.setBounds(125, 59, 95, 21);
 		panelGenerarEmpleado.add(lblTester);
 		
 		cantProgramador = new JSpinner();
 		cantProgramador.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
-		cantProgramador.setBounds(10, 81, 83, 25);
+		cantProgramador.setBounds(10, 83, 83, 25);
 		panelGenerarEmpleado.add(cantProgramador);
 		
 		cantLiderDeProyecto = new JSpinner();
 		cantLiderDeProyecto.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
-		cantLiderDeProyecto.setBounds(10, 28, 83, 25);
+		cantLiderDeProyecto.setBounds(10, 30, 83, 25);
 		panelGenerarEmpleado.add(cantLiderDeProyecto);
 		
 		cantArquitecto = new JSpinner();
 		cantArquitecto.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
-		cantArquitecto.setBounds(113, 28, 83, 25);
+		cantArquitecto.setBounds(125, 30, 83, 25);
 		panelGenerarEmpleado.add(cantArquitecto);
 		
 		cantTester = new JSpinner();
 		cantTester.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
-		cantTester.setBounds(113, 81, 83, 25);
+		cantTester.setBounds(125, 83, 83, 25);
 		panelGenerarEmpleado.add(cantTester);
 		
 		btnGenerar = new JButton("Generar");
-		btnGenerar.setBounds(110, 137, 86, 30);
+		btnGenerar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) 
+			{
+				btnGenerar.setFont(new Font("Nirmala UI", Font.BOLD, 12));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) 
+			{
+				btnGenerar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+			}
+		});
+		btnGenerar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+		btnGenerar.setBackground(verde2.darker());
+		btnGenerar.setForeground(Color.WHITE);
+		btnGenerar.setBounds(125, 130, 83, 30);
+		btnGenerar.setFocusable(false);
 		panelGenerarEmpleado.add(btnGenerar);
 		
 		lblCantNombres = new JLabel("");
+		lblCantNombres.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) 
+			{
+				lblCantNombres.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/iconos/advertencia32px.png")));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				lblCantNombres.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/iconos/advertencia24px.png")));
+			}
+		});
 		lblCantNombres.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCantNombres.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/iconos/aviso.png")));
-		lblCantNombres.setBounds(30, 125, 46, 30);
+		lblCantNombres.setIcon(new ImageIcon(VentanaPrincipal.class.getResource("/iconos/advertencia24px.png")));
+		lblCantNombres.setBounds(35, 125, 40, 40);
 		panelGenerarEmpleado.add(lblCantNombres);	
 		
+		btnEliminar = new JButton("Eliminar");
+		btnEliminar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent arg0) 
+			{
+				btnEliminar.setFont(new Font("Nirmala UI", Font.BOLD, 12));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) 
+			{
+				btnEliminar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+			}
+		});
+		btnEliminar.setFont(new Font("Nirmala UI", Font.BOLD, 13));
+		btnEliminar.setBounds(64, 426, 86, 30);
+		btnEliminar.setBackground(verde2.darker());
+		btnEliminar.setForeground(Color.WHITE);
+		btnEliminar.setEnabled(false);
+		btnEliminar.setFocusable(false);
+		getContentPane().add(btnEliminar);
+		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 200, 564, 256);
+		scrollPane.setBounds(238, 0, 496, 508);
 		getContentPane().add(scrollPane);
 		
 		tablaEmpleados = new JTable(new Object[][] {}, new String[] {"Nombre", "Puesto"});
