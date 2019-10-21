e8214226018b288d331f71238fe918b6e3319ee3
AndresJava <54609896+AndresJava@users.noreply.github.com>
2019-10-20 23:07:19 -0300

    Primera implementacion

--- TP-3/src/modelo/Modelo.java
+++ TP-3/src/modelo/Modelo.java
@@ -1,11 +1,29 @@
 package modelo;
 
+
 public class Modelo 
 {
+	Grafo relaciones;
 	
 	public Modelo() 
 	{
-		
+	}
+	
+	public void inicializar(int cantidad)
+	{
+		relaciones = new Grafo(cantidad);
+	}
+	
+	public void agregarConflictoEntre(int e1, int e2)
+	{
+		relaciones.agregarArista(e1,e2);
+	}
+	
+	public boolean existeMalaRelacion(int e1, int e2)
+	{
+		return relaciones.existeArista(e1, e2);
 	}
 
+	
+	
 }
