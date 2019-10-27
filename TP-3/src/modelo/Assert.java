1b178a037d4f7199ecb9fbdbde8afecc08078e95
AndresJava <54609896+AndresJava@users.noreply.github.com>
2019-10-26 22:15:26 -0300

    Assert ahora compara exclusivamente Empleados

--- TP-3/src/modelo/Assert.java
+++ TP-3/src/modelo/Assert.java
@@ -3,15 +3,15 @@ package modelo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Set;
+import java.util.ArrayList;
 
 public class Assert
 {
-	public static void iguales(int[] expected, Set<Integer> obtained)
+	public static void iguales(ArrayList<Empleado> expected, ArrayList<Empleado> obtained)
 	{
-		assertEquals(expected.length, obtained.size());
+		assertEquals(expected.size(), obtained.size());
 		
-		for(Integer valor: expected)
-			assertTrue( obtained.contains(valor) );
+		for(Empleado empleado: expected)
+			assertTrue( obtained.contains(empleado) );
 	}
 }
